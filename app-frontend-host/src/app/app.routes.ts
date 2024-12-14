e9aabf2497083f23cc24bdf0383383df9085c3ec
sohjunjie <junjie.soh93@gmail.com>
2024-12-14 22:51:18 +0800

    use angular 18

--- app-frontend-host/src/app/app.routes.ts
+++ app-frontend-host/src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
-import { RemoteLoadComponent } from './remote-load/remote-load.component';
-import { RemotePageComponent } from './remote-page/remote-page.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
-    { path: 'remote-load', component: RemoteLoadComponent},
-    { path: 'remote-page', component: RemotePageComponent}
+    {
+        path: '',
+        redirectTo: 'app',
+        pathMatch: 'full'
+    },
+    {
+        path: 'app',
+        component: AppComponent,
+        children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: 'remote-page', loadComponent: () => loadRemoteModule('app-frontend-remote', './Component')
+                .then((m) => m.AppComponent)
+            }
+        ]
+    }
 ];
